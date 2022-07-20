54d221989911e1a67149ddc86955237580b0771a
von_Krolikoff <milkbrother@yandex.ru>
2022-07-20 20:32:42 +0300

    +EX01

--- EX01/Program.cs
+++ EX01/Program.cs
@@ -1,31 +1,62 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-int[,] array = new int[5, 5];
-int x = 0;
+Console.WriteLine("Введите размер массива mXn:");
+int m = Input("Введите m: ");
+int n = Input("Введите n: ");
+
+int[,] array = new int[m, n];
+Console.WriteLine("Создан массив:");
+PourArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив отсортирован:");
+SortArrayLines(array);
+PrintArray(array);
+
+int Input(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
 
 void PourArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
+      array[i, j] = new Random().Next(1, 10);
     }
+  }
 }
 
 void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0);i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
+      Console.Write(array[i, j] + " ");
     }
+    Console.WriteLine();
+  }
 }
 
-PourArray(array);
-PrintArray(array);
-Console.WriteLine();
-
+void SortArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
