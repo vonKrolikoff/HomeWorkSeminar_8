adce9450a60cdddf505f1956d6e11dbe9407282e
von_Krolikoff <milkbrother@yandex.ru>
2022-07-20 20:21:55 +0300

    +EX01

---
+++ EX01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] array = new int[5, 5];
+int x = 0;
+
+void PourArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0);i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PourArray(array);
+PrintArray(array);
+Console.WriteLine();
+
